6db25792c24d875c673a0288acd54439230505fc
drewpendergrass <pendergassdrew@gmail.com>
2021-09-11 15:06:57 -0400

    bug fix

--- testing/test_sequence.sh
+++ testing/test_sequence.sh
@@ -7,10 +7,10 @@ if [ $1 -eq "SETUP" ]; then
 else
 	RUN_NAME="$(jq -r ".RUN_NAME" test_config.json)"
 	MY_PATH="$(jq -r ".MY_PATH" test_config.json)"
-	rm -rf ${RUN_NAME}/${MY_PATH}/ensemble_runs/logs/*
-	rm -rf ${RUN_NAME}/${MY_PATH}/scratch/*.CHECK
-	rm -rf ${RUN_NAME}/${MY_PATH}/scratch/*.npy
-	rm ${RUN_NAME}/${MY_PATH}/KILL_ENS
+	rm -rf ${MY_PATH}/${RUN_NAME}/ensemble_runs/logs/*
+	rm -rf ${MY_PATH}/${RUN_NAME}/scratch/*.CHECK
+	rm -rf ${MY_PATH}/${RUN_NAME}/scratch/*.npy
+	rm ${MY_PATH}/${RUN_NAME}/scratch/KILL_ENS
 fi
 
 bash autotest_one_core.sh
