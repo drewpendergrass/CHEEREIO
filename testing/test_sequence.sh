fd5d2eaa2aa17e31619a49633f28c29a14d73d9a
drewpendergrass <pendergassdrew@gmail.com>
2021-09-12 10:34:30 -0400

    added print statements

--- testing/test_sequence.sh
+++ testing/test_sequence.sh
@@ -3,8 +3,10 @@
 #If command arg is SETUP, setup the testdir; otherwise, just clean up
 
 if [ "$1" = "SETUP" ]; then
+	printf "Reinstalling test directory...\n"
 	bash setup_test_dir.sh
 else
+	printf "Cleaning test directory...\n"
 	RUN_NAME="$(jq -r ".RUN_NAME" test_config.json)"
 	MY_PATH="$(jq -r ".MY_PATH" test_config.json)"
 	rm -rf ${MY_PATH}/${RUN_NAME}/ensemble_runs/logs/*
@@ -13,4 +15,5 @@ else
 	rm ${MY_PATH}/${RUN_NAME}/scratch/KILL_ENS
 fi
 
+printf "Test directory ready. Begin autotest for one core.\n"
 bash autotest_one_core.sh
