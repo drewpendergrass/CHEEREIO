9769dc2c6dc59319637292adb0cc13e5e862604b
drewpendergrass <pendergassdrew@gmail.com>
2021-09-14 17:20:51 -0400

    updated test sequence removals

--- testing/test_sequence.sh
+++ testing/test_sequence.sh
@@ -10,9 +10,12 @@ else
 	RUN_NAME="$(jq -r ".RUN_NAME" test_config.json)"
 	MY_PATH="$(jq -r ".MY_PATH" test_config.json)"
 	rm -rf ${MY_PATH}/${RUN_NAME}/ensemble_runs/logs/*
-	rm -rf ${MY_PATH}/${RUN_NAME}/scratch/*.CHECK
 	rm -rf ${MY_PATH}/${RUN_NAME}/scratch/*.npy
 	rm ${MY_PATH}/${RUN_NAME}/scratch/KILL_ENS
+	rm ${MY_PATH}/${RUN_NAME}/scratch/ENSEMBLE_COMPLETE
+	rm ${MY_PATH}/${RUN_NAME}/scratch/ASSIMILATION_COMPLETE
+	rm ${MY_PATH}/${RUN_NAME}/scratch/ALL_RUNS_COMPLETE
+	rm ${MY_PATH}/${RUN_NAME}/scratch/ALL_COLUMNS_FOUND
 fi
 
 printf "Test directory ready. Begin autotest for one core.\n"
