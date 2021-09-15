06d8a24cc8d38b9cdc0ccf4610bf58be697957eb
Drew Pendergrass <pendergrassdrew@gmail.com>
2021-09-15 09:13:50 -0400

    added diffing to test sequence

---
+++ core/diff_col.py
@@ -0,0 +1,30 @@
+import sys 
+import letkf_utils as lu
+import toolbox as tx 
+
+#Called like so: python diff_col.py TIMESTAMP TESTSTRING LATIND LONIND
+
+timestamp = str(sys.argv[1]) #Time to assimilate. Expected in form YYYYMMDD_HHMM, UTC time.
+
+cmdarg = str(sys.argv[2])
+if cmdarg=="TESTING":
+	data = tx.getSpeciesConfig(testing=True)
+else:
+	data = tx.getSpeciesConfig(testing=False)
+
+latind = int(sys.argv[3])
+lonind = int(sys.argv[4])
+dateval = timestamp[0:4]+'-'+timestamp[4:6]+'-'+timestamp[6:8]
+
+control_conc = data['CONTROL_VECTOR_CONC']
+control_emis = list(data['CONTROL_VECTOR_EMIS'].keys())
+
+wrapper = lu.GT_Container(timestamp,testing)
+
+print(f'AUTOTESTING CONTROL VECTOR SPECIES AT {(latind,lonind)}')
+for species in control_conc:
+	wrapper.compareSpeciesConc(species,latind,lonind)
+
+print(f'AUTOTESTING CONTROL VECTOR EMISSIONS AT {(latind,lonind)}')
+for species in control_emis:
+	wrapper.compareSpeciesEmis(species,latind,lonind)
--- testing/autotest_one_core.sh
+++ testing/autotest_one_core.sh
@@ -2,3 +2,6 @@
 
 cd ../core
 bash par_assim.sh true 1 1
+cd ../testing
+bash diffcolumn.sh 0 0
+bash diffcolumn.sh
