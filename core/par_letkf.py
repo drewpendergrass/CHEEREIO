b3e1d1b17710e4e27bd9a3301ce209f165627a66
drewpendergrass <pendergassdrew@gmail.com>
2021-09-17 16:26:06 -0400

    added some commented out lines for testing

--- core/par_letkf.py
+++ core/par_letkf.py
@@ -25,3 +25,21 @@ print(f'Core ({ensnum},{corenum}) completed computation for {dateval} and saved 
 # print(f'Saving completed in {end - start} seconds.')
 # print(f'Core ({ensnum},{corenum}) processed its subset of data for {dateval}.')
 print('-------------------END LETKF-------------------')
+
+#THESE LINES ARE ONLY USED FOR TESTING
+# import letkf_utils as lu
+# import toolbox as tx
+# import numpy as np
+# timestamp = '20190101_0000'
+# ensnum = 1
+# corenum = 1
+# testing = True
+# assimilator = lu.Assimilator(timestamp,ensnum,corenum,testing)
+# assimilator.prepareMeansAndPerts(23,0)
+# assimilator.ybar_background-assimilator.Ypert_background[:,0]
+# latinds,loninds = tx.getIndsOfInterest(23,0,testing=True)
+# conc4D=assimilator.combineEnsembleForSpecies('NO2')
+# obsEns = np.zeros([len(assimilator.ObsOp['NO2_SATELLITE'].obsinfo.getObsVal(23,0)),np.shape(conc4D)[3]])
+
+# for i in range(16):
+# 	obsEns[:,i],_,_ = assimilator.ObsOp['NO2_SATELLITE'].H(conc4D[:,:,:,i],latinds,loninds)
