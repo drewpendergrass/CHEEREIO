80d0a78317c43e517cbde6f53edcd9fe78ac5d43
drewpendergrass <pendergassdrew@gmail.com>
2021-09-19 09:29:59 -0400

    have to do string subs for parallel because of weird environment things

--- templates/run_ensemble_simulations.sh
+++ templates/run_ensemble_simulations.sh
@@ -44,7 +44,7 @@ cd  {RunName}_${xstr}
 
 # Set the proper # of threads for OpenMP
 # SLURM_CPUS_PER_TASK ensures this matches the number you set with NumCores in the ens_config file
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_NUM_THREADS={NumCores}
 
 #Run GC; hang until assimilation complete (later also will do assimilation).
 #This will loop until a file appears in scratch signalling assimilation is complete.
@@ -90,7 +90,7 @@ while [ ! -f ${MY_PATH}/${RUN_NAME}/scratch/ENSEMBLE_COMPLETE ]; do
   #CD to core
   cd {ASSIM}/core
   #Use GNU parallel to submit parallel sruns
-  parallel -j ${SLURM_CPUS_PER_TASK} "srun -n1 -N1 --exclusive bash par_assim.sh ${TESTING} ${x} {1}" ::: {1..${SLURM_CPUS_PER_TASK}}
+  parallel -j ${SLURM_CPUS_PER_TASK} "bash par_assim.sh ${TESTING} ${x} {1}" ::: {1..{NumCores}}
   #Hang until assimilation completes or cleanup completes (in case things go too quickly)
   until [ -f ${MY_PATH}/${RUN_NAME}/scratch/ASSIMILATION_COMPLETE ] || [ ! -f ${MY_PATH}/${RUN_NAME}/scratch/ALL_RUNS_COMPLETE ]; do
     #If this is ensemble member 1, check if assimilation is complete; if it is, do the final overwrites.
