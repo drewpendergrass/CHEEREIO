1abfc363bc002555d66b7c28c41d82a4799e926f
drewpendergrass <pendergassdrew@gmail.com>
2021-09-19 11:19:34 -0400

    switch

--- setup_ensemble.sh
+++ setup_ensemble.sh
@@ -16,9 +16,9 @@
 # Turn on/off different steps. This will allow you to come back to this
 # script and set up different stages later.
 SetupTemplateRundir=true
-CompileTemplateRundir=true
+CompileTemplateRundir=false
 SetupSpinupRun=false
-SetupEnsembleRuns=true
+SetupEnsembleRuns=false
 
 # Path to assimilation setup
 ASSIM_PATH="$(jq -r ".ASSIM_PATH" ens_config.json)"
