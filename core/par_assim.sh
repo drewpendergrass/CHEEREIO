ea4032b04d6a081735137bf7aa6b1855931d1881
Drew Pendergrass <pendergrassdrew@gmail.com>
2021-09-19 16:49:41 -0400

    try bug fix

--- core/par_assim.sh
+++ core/par_assim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Figure out timestamp for assimilation
-if [ "${1}" = true ]; then
+if [ "${1}" = "true" ]; then
 	MY_PATH="$(jq -r ".MY_PATH" ../testing/test_config.json)"
 	RUN_NAME="$(jq -r ".RUN_NAME" ../testing/test_config.json)"
 	CONDA_ENV=$(jq -r ".CondaEnv" ../testing/test_config.json)
