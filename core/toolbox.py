17bd91c7f1bb572b75c87e2cf3c516d1c36ba43e
Drew Pendergrass <pendergrassdrew@gmail.com>
2021-09-24 10:34:14 -0400

    sped up toolbox

--- core/toolbox.py
+++ core/toolbox.py
@@ -30,7 +30,7 @@ def getLatLonVals(data=None,testing=False):
 def calcDist_km_fromscratch(lat1,lon1,lat2,lon2):
 	coords_1 = (lat1,lon1)
 	coords_2 = (lat2,lon2)
-	d = gd.distance(coords_1, coords_2).km
+	d = gd.great_circle(coords_1, coords_2).km #This approximation is fine.
 	return d
 
 #Inputs are in degrees
