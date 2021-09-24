62f0d2335cac56debe51bf45f5798f533501e646
Drew Pendergrass <pendergrassdrew@gmail.com>
2021-09-24 17:18:03 -0400

    update environment

--- core/check_and_complete_assimilation.sh
+++ core/check_and_complete_assimilation.sh
@@ -18,11 +18,12 @@ end_timestamp="$(tail -n 1 ${MY_PATH}/${RUN_NAME}/scratch/INPUT_GEOS_TEMP)"
 end_timestamp="${end_timestamp%??}" #Clear last two characters
 end_timestamp="${end_timestamp// /_}" #Replace space with underscore
 
+source activate ${CONDA_ENV} #Activate conda environment.
 python check_for_all_columns.py ${TESTSTR}
 
 if [ -f ${MY_PATH}/${RUN_NAME}/scratch/ALL_COLUMNS_FOUND ]; then
-	source activate ${CONDA_ENV} #Activate conda environment.
 	python combine_columns_and_update.py ${end_timestamp} ${TESTSTR} >> ${MY_PATH}/${RUN_NAME}/ensemble_runs/logs/letkf_master.out
-	conda deactivate #Exit Conda environment
 	echo 'Done' > ${MY_PATH}/${RUN_NAME}/scratch/ASSIMILATION_COMPLETE
 fi
+
+conda deactivate #Exit Conda environment
