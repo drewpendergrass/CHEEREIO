1333a934247c554ffe7f8faa1f749f963cf75899
Drew Pendergrass <pendergrassdrew@gmail.com>
2021-10-02 16:02:35 -0400

    update

--- postprocess/postprocess_workflow.py
+++ postprocess/postprocess_workflow.py
@@ -32,6 +32,6 @@ except FileNotFoundError:
 	ds = xr.open_dataset(f'{pp_dir}/controlvar_pp.nc')
 
 for spec in controlvec:
-	pt.plotSurfaceCell(ds,spec,30,59,outfile=f'{pp_dir}/wuhan_cell_ts_{spec}.png',unit='ppm',includesNature=True)
-	pt.plotSurfaceMean(ds,spec,outfile=f'{pp_dir}/surfmean_ts_{spec}.png',unit='ppm',includesNature=True)
+	pt.plotSurfaceCell(ds,spec,30,59,outfile=f'{pp_dir}/wuhan_cell_ts_{spec}.png',includesNature=True)
+	pt.plotSurfaceMean(ds,spec,outfile=f'{pp_dir}/surfmean_ts_{spec}.png',includesNature=True)
 
