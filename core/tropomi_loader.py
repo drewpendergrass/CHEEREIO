34d4b114fb86438d9e9fc42c5516083e5b9526d4
drewpendergrass <pendergassdrew@gmail.com>
2021-11-07 09:43:55 -0500

    update

--- core/tropomi_loader.py
+++ core/tropomi_loader.py
@@ -1,6 +1,7 @@
 import tropomi_tools as tt
 from datetime import datetime
 
+trans = tt.TROPOMI_Translator()
 trans.initialReadDate()
 timeperiod = [datetime.strptime('2019-01-01 00:00', '%Y-%m-%d %H:%M'), datetime.strptime('2019-01-07 23:59', '%Y-%m-%d %H:%M')]
 
