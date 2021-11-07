2ad5ea4b2a87d47506324b0b7ec2e805252b9861
drewpendergrass <pendergassdrew@gmail.com>
2021-11-07 09:42:12 -0500

    wrote test loader

---
+++ core/tropomi_loader.py
@@ -0,0 +1,7 @@
+import tropomi_tools as tt
+from datetime import datetime
+
+trans.initialReadDate()
+timeperiod = [datetime.strptime('2019-01-01 00:00', '%Y-%m-%d %H:%M'), datetime.strptime('2019-01-07 23:59', '%Y-%m-%d %H:%M')]
+
+trop_dat = trans.getTROPOMI('CH4',timeperiod)
