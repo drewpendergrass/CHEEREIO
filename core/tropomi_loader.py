13d0e40a0b13c8a8d38f33d9007ac49a3437cc94
drewpendergrass <pendergassdrew@gmail.com>
2021-11-07 10:57:21 -0500

    ready for testing gc loader

--- core/tropomi_loader.py
+++ core/tropomi_loader.py
@@ -1,10 +1,14 @@
 import tropomi_tools as tt
+import letkf_utils as lu
 from datetime import datetime
 
 trans = tt.TROPOMI_Translator()
 timeperiod = [datetime.strptime('2019-01-01 00:00', '%Y-%m-%d %H:%M'), datetime.strptime('2019-01-07 23:59', '%Y-%m-%d %H:%M')]
 
 trop_dat = trans.getTROPOMI('CH4',timeperiod)
+
+hist_trans = lu.HIST_Translator('/n/holyscratch01/jacob_lab/dpendergrass/GC-LETKF/test_4x5/','20190102_0000')
+gc = hist_trans.combineHist('CH4',True)
 
 # sourcedir = trans.spc_config['TROPOMI_dirs']['CH4']
 # obs_list = glob(f'{sourcedir}/S5P_*.nc')
