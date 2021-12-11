15cf5fb782d2879bf6c0cdce15299223754b903c
Drew Pendergrass <pendergrassdrew@gmail.com>
2021-12-11 09:50:00 -0500

    added file recognition

--- postprocess/postprocess_workflow.py
+++ postprocess/postprocess_workflow.py
@@ -37,7 +37,11 @@ except FileNotFoundError:
 	ds = xr.open_dataset(f'{pp_dir}/controlvar_pp.nc')
 
 if "histprocess" in sys.argv:
-	bigy = pt.makeYWholePeriod(timestamp=f"{data['END_DATE']}_0000",fullpath_output_name=f"{pp_dir}/bigY.pkl")
+	try:
+		with open(f"{pp_dir}/bigY.pkl",'rb') as f:
+			bigy=pickle.load(f)
+	except FileNotFoundError:
+		bigy = pt.makeYWholePeriod(timestamp=f"{data['END_DATE']}_0000",fullpath_output_name=f"{pp_dir}/bigY.pkl")
 
 if "calccol" in sys.argv:
 	try:
@@ -61,12 +65,9 @@ if "calc850" in sys.argv:
 		_ = pt.makeDatasetForEnsemble(ens_dir,controlvec,timeperiod,subset_rule="850",fullpath_output_name=f'{pp_dir}/controlvar_pp_850hPa.nc')
 		ds850 = xr.open_dataset(f'{pp_dir}/controlvar_pp_850hPa.nc')
 
-if "plotsatellite" in sys.argv:
-	with open(f"{pp_dir}/bigY.pkl",'rb') as f:
-		bigy=pickle.load(f)
 
 for spec in controlvec:
-	if "plotsatellite" in sys.argv:
+	if "histprocess" in sys.argv:
 		df = bigy['spec']
 		pt.tsPlotSatCompare(df,spec,nEnsemble,outfile=f'{pp_dir}/satellite_ts_compare_{spec}.png')
 	pt.plotSurfaceCell(ds,spec,30,59,outfile=f'{pp_dir}/wuhan_cell_ts_{spec}.png',includesNature=True)
