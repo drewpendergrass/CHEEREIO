84120294cd46d8ee483ed6132c54a95e35cbaf35
Drew Pendergrass <pendergrassdrew@gmail.com>
2021-12-15 10:12:13 -0600

    update initialization masking

--- core/initialize_scaling_factors.py
+++ core/initialize_scaling_factors.py
@@ -42,6 +42,8 @@ if len(spc_config["REGION"])==0:
 else:
 	gridlabel = f'{spc_config["REGION"]}_{spc_config["met_name"]}'
 
+subhalf = False
+subquarter = False
 #Create traditional GEOS-Chem longitude and latitude centers, as specified by the settings in ens_config.json
 if gridlabel == '4.0x5.0':
 	lon = np.arange(-180.0,176.0, 5.0)
@@ -58,32 +60,62 @@ elif gridlabel == '2.0x2.5':
 elif gridlabel == '1x1':
 	lon = np.arange(-179.5,180.0, 1.0)
 	lat = np.arange(-89.5,90, 1.0)
+	mask = pd.read_csv('../templates/landmask_1x1_gcgrid.csv',header=None)
+	mask = np.array(np.transpose(mask))
+	mask = np.where(mask==0)
 elif (gridlabel == '0.5x0.625') | (gridlabel == 'MERRA2'): #MERRA2 NATIVE GRID
 	lon = -180.0 + (0.625*np.arange(0.0,576.0,1.0))
 	lat = -90.0 + (0.5*np.arange(0.0,361.0,1.0))
+	mask = pd.read_csv('../templates/landmask_0p5x0p625_gcgrid.csv',header=None)
+	mask = np.array(np.transpose(mask))
+	mask = np.where(mask==0)
 elif (gridlabel == 'AS_MERRA2'): #ASIA NESTED GRID FOR MERRA2
 	lon = np.arange(60.0,150.01, 0.625)
 	lat = np.arange(-11.0,55.01, 0.5)
+	subhalf = True
 elif (gridlabel == 'EU_MERRA2'): #EUROPE NESTED GRID FOR MERRA2
 	lon = np.arange(-30.0,50.01, 0.625)
 	lat = np.arange(30.0,70.01, 0.5)
+	subhalf = True
 elif (gridlabel == 'NA_MERRA2'): #NORTH AMERICA NESTED GRID FOR MERRA2
 	lon = np.arange(-140.0,-39.99, 0.625)
 	lat = np.arange(10.0,70.01, 0.5)
+	subhalf = True
 elif (gridlabel == '0.25x0.3125') | (gridlabel == 'GEOSFP'):
 	lon = -180.0 + (0.3125*np.arange(0.0,1152.0,1.0))
 	lat = -90.0 + (0.25*np.arange(0.0,721.0,1.0))
+	mask = pd.read_csv('../templates/landmask_0p25x0p3125_gcgrid.csv',header=None)
+	mask = np.array(np.transpose(mask))
+	mask = np.where(mask==0)
 elif (gridlabel == 'CH_GEOSFP'): #CHINA NESTED GRID FOR GEOS-FP
 	lon = np.arange(70.0,140.01, 0.3125)
 	lat = np.arange(15.0,55.01, 0.25)
+	subquarter = True
 elif (gridlabel == 'EU_GEOSFP'): #EU NESTED GRID FOR GEOS-FP
 	lon = np.arange(-15.0,40.01, 0.3125)
 	lat = np.arange(32.75,61.26, 0.25)
+	subquarter = True
 elif (gridlabel == 'NA_GEOSFP'): #NA NESTED GRID FOR GEOS-FP
 	lon = np.arange(-130.0,-59.99, 0.3125)
 	lat = np.arange(9.75,60.01, 0.25)
+	subquarter = True
 else:
 	raise ValueError('Scaling factor initialization utility does not recognize grid specification.')
+
+if subhalf or subquarter:
+	if subhalf:
+		fulllon = -180.0 + (0.625*np.arange(0.0,576.0,1.0))
+		fulllat = -90.0 + (0.5*np.arange(0.0,361.0,1.0))
+		mask = pd.read_csv('../templates/landmask_0p5x0p625_gcgrid.csv',header=None)
+	elif subquarter:
+		fulllon = -180.0 + (0.3125*np.arange(0.0,1152.0,1.0))
+		fulllat = -90.0 + (0.25*np.arange(0.0,721.0,1.0))
+		mask = pd.read_csv('../templates/landmask_0p25x0p3125_gcgrid.csv',header=None)
+	lonok = np.where((fulllon>=np.min(lon)) & (fulllon<=np.max(lon)))[0]
+	latok = np.where((fulllat>=np.min(lat)) & (fulllat<=np.max(lat)))[0]
+	mask = np.array(np.transpose(mask))
+	mask = mask[lonok,latok]
+	mask = np.where(mask==0)
 
 perturbation = float(spc_config["pPERT"]) #perturbation, max positive amount. i.e. if it is 4 scaling factors will range between 0.25 and 4.
 
