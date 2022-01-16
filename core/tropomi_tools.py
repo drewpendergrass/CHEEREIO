bug fix on indexing in averaging TROPOMI to GC grid

core/tropomi_tools.py
@@ -213,11 +213,14 @@ def averageByGCTROPOMIlocs(index,GConsat,satvals,satlat,satlon,sattime):
 
 #No index, puts loc at GC grid values
 def averageByGC(iGC, jGC, tGC, GC,GConsat,satvals,satlat,satlon,sattime):
-	index = (iGC*100000000)+(jGC*10000)+tGC
+	index = ((iGC+1)*100000000)+((jGC+1)*10000)+(tGC+1)
 	unique_inds = np.unique(index)
-	lonvals = GC.lon.values[iGC[np.floor(unique_inds/100000000).astype(int)]]
-	latvals = GC.lat.values[jGC[(np.floor(unique_inds/10000).astype(int) % 10000)]]
-	timevals = GC.time.values[tGC[(unique_inds % 10000).astype(int)]]
+	i_unique = np.floor(unique_inds/100000000).astype(int)-1
+	j_unique = (np.floor(unique_inds/10000).astype(int) % 10000)-1
+	t_unique = (unique_inds % 10000).astype(int)-1
+	lonvals = GC.lon.values[i_unique]
+	latvals = GC.lat.values[j_unique]
+	timevals = GC.time.values[t_unique]
 	av_len = len(unique_inds)
 	gc_av = np.zeros(av_len)
 	sat_av = np.zeros(av_len)

postprocess/postprocess_tools.py
@@ -117,6 +117,8 @@ def makeYEachAssimPeriod(timestamp_list, use_numav = True, fullpath_output_name 
 		hist = lu.HIST_Ens(timestamp=timestamp,useLevelEdge=True,testing=False)
 		bigy = hist.bigYDict 
 		for spec in list(bigy.keys()):
+			t = [np.datetime64(int(tt),'ns') for tt in bigy[spec][4]]
+			t = np.array(t,dtype='datetime64[us]')
 			colnum = np.shape(bigy[spec][0])[1]
 			colnames = []
 			for i in range(colnum):
@@ -126,9 +128,10 @@ def makeYEachAssimPeriod(timestamp_list, use_numav = True, fullpath_output_name 
 			df['Latitude'] = bigy[spec][2]
 			df['Longitude'] = bigy[spec][3]
 			if use_numav:
-				df['Num_Averaged'] = bigy[spec][4]
+				df['Num_Averaged'] = bigy[spec][5]
 			else:
 				df['Num_Averaged'] = None
+			df['time'] = t
 			bigy[spec] = df
 		masterY[timestamp] = bigy
 	if fullpath_output_name: