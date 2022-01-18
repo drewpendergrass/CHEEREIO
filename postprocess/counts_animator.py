0b033e88d5922920ab30574f1d1c1f3d3520245c
Drew Pendergrass <pendergrassdrew@gmail.com>
2022-01-18 17:15:10 -0500

    prep for plotting counts

---
+++ postprocess/counts_animator.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from mpl_toolkits.basemap import Basemap
+import pickle
+import json
+
+#with open('../ens_config.json') as f:
+with open('/n/home12/drewpendergrass/CHEEREIO/ens_config.json') as f:
+	data = json.load(f)
+
+data['RUN_NAME'] = 'METHANERUN_v8'
+pp_dir = f"{data['MY_PATH']}/{data['RUN_NAME']}/postprocess"
+
+with open(f'{pp_dir}/count_arrays_for_plotting.pkl','rb') as f:
+	pickledata=pickle.load(f)
+
+arraysbase = pickledata[0:2]
+dates = pickledata[2]
+specieslist = pickledata[3]
+
+with open(f"{data['MY_PATH']}/{data['RUN_NAME']}/scratch/latlon_vals.json") as f:
+	ll_data = json.load(f)
+
+gclat = np.array(ll_data['lat'])
+gclon = np.array(ll_data['lon'])
+
+filenamesbase = [f'{pp_dir}/total_raw_satellite_counts',f'{pp_dir}/total_averaged_satellite_counts']
+
+for arrayval,filenamebase in zip(arraysbase,filenamesbase):
+	for ind,species in enumerate(specieslist):
+		def animate(i):
+			daystring = dates[i]
+			titlestring = f'{daystring}'
+			plt.title(titlestring)
+			temp = arrayval[i,ind,:,:]
+			temp = temp[:-1, :-1] #weird old bug fix found on stackoverflow
+			#mesh = m.pcolormesh(lon, lat, maptimeseries[:,:,i],latlon=True)
+			mesh.set_array(temp.ravel())
+			return mesh
+		# call the animator.  blit=True means only re-draw the parts that have changed.
+		fig = plt.figure(figsize=(10, 8))
+		m = Basemap(projection='cyl', resolution='l',llcrnrlat=-90, urcrnrlat=90,llcrnrlon=-180, urcrnrlon=180)
+		m.drawcountries(color='lightgray')
+		m.drawcoastlines(color='lightgray')
+		mesh = m.pcolormesh(gclon, gclat, arrayval[0,ind,:,:],latlon=True,cmap=plt.cm.jet)
+		plt.clim(np.min(arrayval[:,ind,:,:]), np.max(arrayval[:,ind,:,:]))
+		plt.colorbar(label="Count");
+		anim = animation.FuncAnimation(fig, animate,len(dates), blit=False)
+		Writer = animation.writers['ffmpeg']
+		writer = Writer(fps=anim_fps, metadata=dict(artist='Drew Pendergrass'), bitrate=800) #low res, small memory plot
+		anim.save(f'{filenamebase}_{species}.mp4', writer=writer)
---
+++ postprocess/counts_prep.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import pickle
+import json
+
+#with open('../ens_config.json') as f:
+with open('/n/home12/drewpendergrass/CHEEREIO/ens_config.json') as f:
+	data = json.load(f)
+
+data['RUN_NAME'] = 'METHANERUN_v8'
+pp_dir = f"{data['MY_PATH']}/{data['RUN_NAME']}/postprocess"
+
+with open(f"{pp_dir}/bigY.pkl",'rb') as f:
+	bigy=pickle.load(f)
+
+with open(f"{data['MY_PATH']}/{data['RUN_NAME']}/scratch/latlon_vals.json") as f:
+	ll_data = json.load(f)
+
+gclat = np.array(ll_data['lat'])
+gclon = np.array(ll_data['lon'])
+
+dates = list(bigy.keys())
+specieslist = list(bigy[dates[0]].keys())
+
+total_satellite_obs = np.zeros([len(dates),len(specieslist),len(gclon),len(gclat)])
+total_averaged_obs = np.zeros([len(dates),len(specieslist),len(gclon),len(gclat)])
+
+for ind1, date in enumerate(dates):
+	daydict = bigy[date]
+	for ind2, species in enumerate(specieslist):
+		ydict = daydict[species]
+		latdict = np.array(ydict['Latitude'])
+		londict = np.array(ydict['Longitude'])
+		countdict = np.array(ydict['Num_Averaged'])
+		latind = np.abs(latdict.reshape((1, -1)) - gclat.reshape((-1, 1)))
+		latind = latind.argmin(axis=0)
+		lonind = np.abs(londict.reshape((1, -1)) - gclon.reshape((-1, 1)))
+		lonind = lonind.argmin(axis=0)
+		pairedind = ((latind+1)*10000)+(lonind+1)
+		uniqueind,countind = np.unique(pairedind,return_counts=True)
+		uniquelonind = (uniqueind % 10000)-1
+		uniquelatind = np.floor(uniqueind / 10000).astype(int)-1
+		total_averaged_obs[ind1,ind2,uniquelonind,uniquelatind]=countind
+		for lonindval,latindval in zip(uniquelonind,uniquelatind):
+			dictind = np.where((latdict==gclat[latindval])&(londict==gclon[lonindval]))[0]
+			totalcount = np.sum(countdict[dictind])
+			total_satellite_obs[ind1,ind2,lonindval,latindval]=totalcount
+
+
+arraysbase = [total_satellite_obs,total_averaged_obs,dates,specieslist]
+
+f = open(f'{pp_dir}/count_arrays_for_plotting.pkl',"wb")
+pickle.dump(arraysbase,f)
+f.close()
