d393143c487b02112540cd89f975bde76211e09e
drewpendergrass <pendergassdrew@gmail.com>
2022-03-01 09:43:03 -0500

    removed possibility of runs starting before history.rc update if scratch hangs

--- templates/run_ensemble_simulations.sh
+++ templates/run_ensemble_simulations.sh
@@ -115,11 +115,11 @@ while [ ! -f ${MY_PATH}/${RUN_NAME}/scratch/ENSEMBLE_COMPLETE ]; do
   fi
   #If this is ensemble member 1, execute cleanup. This is because we only want it to run once.
   if [ $x -eq 1 ]; then
-    bash cleanup.sh ${TESTING} #This also will break us out of this loop when assimilation complete.
     if [ "${firstrun}" = true ]; then
       bash change_histrst_durfreq.sh
       firstrun=false
     fi
+    bash cleanup.sh ${TESTING} #This also will break us out of this loop when assimilation complete.
   fi
   #Hang until cleanup complete, as determined by temp file deletion.
   until [ ! -f ${MY_PATH}/${RUN_NAME}/scratch/ASSIMILATION_COMPLETE ]; do
