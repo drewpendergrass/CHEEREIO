d35e1402f1fb16748f1d5e0fcf032e5c2c87d383
Drew Pendergrass <pendergrassdrew@gmail.com>
2022-06-06 17:13:48 -0500

    start of pytest for GC_Translator

--- core/tests/test_GC_Translator.py
+++ core/tests/test_GC_Translator.py
@@ -3,3 +3,8 @@ import pytest
 sys.path.append('../')
 from GC_Translator import GC_Translator
 
+#These tests ensure that we are subsetting columns correctly in the GC_Translator class.
+
+def test_col_subset():
+	gt = GC_Translator('data_for_tests/TEST_0001/','20190101_0000',computeStateVec = True)
+	
