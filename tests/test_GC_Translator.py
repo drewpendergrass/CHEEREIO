d5990b5619601bc8eead2d372f9f0c2385edb8ee
Drew Pendergrass <pendergrassdrew@gmail.com>
2022-06-07 11:22:32 -0500

    reduce atol; add full statevec test

--- tests/test_GC_Translator.py
+++ tests/test_GC_Translator.py
@@ -19,4 +19,19 @@ def test_col_subset_of_localized_state_vector_methane():
 	ds = xr.load_dataset('data_for_tests/METHANE_TEST/TEST_0001/GEOSChem.Restart.20190101_0000z.nc4')
 	da = np.array(ds[f'SpeciesRst_CH4']).squeeze()
 	column_from_file = da[:,10,10]
-	assert np.allclose(column_from_statevec,column_from_file)
+	assert np.allclose(column_from_statevec,column_from_file,atol=1e-10)
+
+#From the methane restart, get column from full state vector.
+#Compare this to the true column from the file
+def test_col_subset_of_full_state_vector_methane():
+	#Get the column from the localized state vector
+	gt = GC_Translator('data_for_tests/METHANE_TEST/TEST_0001/','20190101_0000',computeStateVec = True)
+	statevec = gt.getStateVector()
+	columninds = gt.getColumnIndicesFromFullStateVector(12,16)
+	column_from_statevec = statevec[columninds] #This column will have a scaling factor for the last entry. Remove it.
+	column_from_statevec = column_from_statevec[0:-1] #Cuts off last entry
+	ds = xr.load_dataset('data_for_tests/METHANE_TEST/TEST_0001/GEOSChem.Restart.20190101_0000z.nc4')
+	da = np.array(ds[f'SpeciesRst_CH4']).squeeze()
+	column_from_file = da[:,12,16]
+	assert np.allclose(column_from_statevec,column_from_file,atol=1e-10)
+
