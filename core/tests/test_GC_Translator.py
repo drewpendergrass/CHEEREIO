e81f1b10afd2e3078444c3d0c028412f52bf9f2b
Drew Pendergrass <pendergrassdrew@gmail.com>
2022-06-07 10:14:32 -0500

    added more testing materials including reduced form full chem restart

--- core/tests/test_GC_Translator.py
+++ core/tests/test_GC_Translator.py
@@ -1,5 +1,7 @@
 import sys
 import pytest
+import xarray as xr
+import numpy as np
 sys.path.append('../')
 from GC_Translator import GC_Translator
 
@@ -7,4 +9,5 @@ from GC_Translator import GC_Translator
 
 def test_col_subset():
 	gt = GC_Translator('data_for_tests/TEST_0001/','20190101_0000',computeStateVec = True)
-	
+	ds = xr.load_dataset('data_for_tests/TEST_0001/GEOSChem.Restart.20190101_0000z.nc4')
+	da = np.array(ds[f'SpeciesRst_CH4']).squeeze()
