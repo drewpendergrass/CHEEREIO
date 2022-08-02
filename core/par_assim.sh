b5c8c3889a77e867a1a97f470592d69ac7b20668
Drew Pendergrass <pendergrassdrew@gmail.com>
2022-08-02 09:12:48 -0400

    fix bug in feeding in cmd line arguments

--- core/par_assim.sh
+++ core/par_assim.sh
@@ -11,11 +11,11 @@ end_timestamp="${end_timestamp%??}" #Clear last two characters
 end_timestamp="${end_timestamp// /_}" #Replace space with underscore
     
 source activate ${CONDA_ENV} #Activate conda environment.
-python -u par_letkf.py ${end_timestamp} ${2} ${3} >> ${MY_PATH}/${RUN_NAME}/ensemble_runs/logs/letkf_${2}_${3}.out
+python -u par_letkf.py ${end_timestamp} ${1} ${2} >> ${MY_PATH}/${RUN_NAME}/ensemble_runs/logs/letkf_${1}_${2}.out
 py_exit_status=$?
 conda deactivate #Exit Conda environment
 
 #If python does not exit with exit code one, make file that will break loop
 if [ $py_exit_status != 0 ]; then
-	printf "Python assimilation script exited without code 0 in ensemble ${2} and core ${3} \n" > ${MY_PATH}/${RUN_NAME}/scratch/KILL_ENS #This file's presence will break loop
+	printf "Python assimilation script exited without code 0 in ensemble ${1} and core ${2} \n" > ${MY_PATH}/${RUN_NAME}/scratch/KILL_ENS #This file's presence will break loop
 fi
