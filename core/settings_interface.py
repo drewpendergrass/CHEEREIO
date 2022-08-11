983761bb724de812fefc8cf7bd63cc547972a643
Drew Pendergrass <pendergrassdrew@gmail.com>
2022-08-11 07:48:50 -0400

    split json off from toolbox

---
+++ core/settings_interface.py
@@ -0,0 +1,29 @@
+import json
+
+#Load in settings from ens_config.json. If settings_to_override is active (entry override is True)
+#the overwrite entries provided
+def getSpeciesConfig():
+	with open('../ens_config.json') as f:
+		data = json.load(f)
+	with open('../settings_to_override.json') as f:
+		over_data = json.load(f)
+	if over_data['override'] == "True":
+		for key in list(over_data.keys()):
+			if key != 'override':
+				data[key] = over_data[key]
+	return data
+
+#Get the latitude and longitude list for a particular core (indexed by ensemble and core)
+def getLatLonList(ensnum,corenum):
+	data = getSpeciesConfig()
+	with open(f"{data['MY_PATH']}/{data['RUN_NAME']}/scratch/latlon_par.json") as f:
+		gridsplit = json.load(f)
+	return [gridsplit[f'{ensnum}'][f'{corenum}']['lat'],gridsplit[f'{ensnum}'][f'{corenum}']['lon']]
+
+#Get the lat lon values for the grid from the JSON
+def getLatLonVals(data=None):
+	if not data:
+		data = getSpeciesConfig()
+	with open(f"{data['MY_PATH']}/{data['RUN_NAME']}/scratch/latlon_vals.json") as f:
+		ll_data = json.load(f)
+	return [ll_data['lat'],ll_data['lon']]
