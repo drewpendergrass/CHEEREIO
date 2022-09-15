8eda1e288a6b33d88db8a9424c0d4fa55d4f6cac
Drew Pendergrass <pendergrassdrew@gmail.com>
2022-09-15 07:59:09 -0400

    bug fixes

--- core/par_letkf.py
+++ core/par_letkf.py
@@ -4,11 +4,16 @@ import time
 import settings_interface as si 
 
 data = si.getSpeciesConfig()
+path_to_scratch = f"{data['MY_PATH']}/{data['RUN_NAME']}/scratch"
 timestamp = str(sys.argv[1]) #Time to assimilate. Expected in form YYYYMMDD_HHMM, UTC time.
 ensnum = int(sys.argv[2])
 corenum = int(sys.argv[3])
 just_scale = (str(sys.argv[4])=='true') and (data['SIMPLE_SCALE_FOR_FIRST_ASSIM_PERIOD'] == "true") #If this is the first run AND user specifies, we'll just scale (no assimilation)
-
+if just_scale:
+	label_str = 'scaling'
+else:
+	label_str = 'LETKF'
+label_str
 dateval = timestamp[0:4]+'-'+timestamp[4:6]+'-'+timestamp[6:8]
 
 #If we are just scaling, only ens member 1 and core 1 will do assimilation
@@ -19,15 +24,13 @@ if (not just_scale) or (just_scale and (ensnum == 1) and (corenum==1) ):
 	#a = Assimilator('20190108_0000',2,1)
 	a = Assimilator(timestamp,ensnum,corenum)
 	end = time.time()
-	
+	print(f'Core ({ensnum},{corenum}) gathered ensemble in {end - start} seconds. Begin {label_str} procedure.')
 	start = time.time()
 	if just_scale:
-		print(f'Core ({ensnum},{corenum}) gathered ensemble in {end - start} seconds. Begin scaling procedure.')
 		a.scaleRestarts()
 		with open(f"{path_to_scratch}/ASSIMILATION_COMPLETE", "w") as f:
 			f.write("Done.\n") #If so, save flag file to ensemble folder
 	else:
-		print(f'Core ({ensnum},{corenum}) gathered ensemble in {end - start} seconds. Begin LETKF procedure.')
 		a.LETKF()
 	end = time.time()
 	print(f'Core ({ensnum},{corenum}) completed computation for {dateval} and saved columns in {end - start} seconds.')
