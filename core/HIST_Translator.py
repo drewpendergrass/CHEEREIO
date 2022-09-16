87d3f94157ebad13ad5bc7616f1dcfd9bcf63192
Drew Pendergrass <pendergrassdrew@gmail.com>
2022-09-16 12:00:27 -0400

    generalized the load hist routine; load also all species conc at once to speed things up

--- core/HIST_Translator.py
+++ core/HIST_Translator.py
@@ -12,6 +12,7 @@ class HIST_Translator(object):
 		self.timeperiod = timeperiod
 		self.interval = interval
 	def globSubDir(self,timeperiod,useLevelEdge = False, useStateMet = False):
+		subdir_lists = {}
 		specconc_list = glob(f'{self.hist_dir}/GEOSChem.SpeciesConc*.nc4')
 		specconc_list.sort()
 		ts = [datetime.strptime(spc.split('.')[-2][0:13], "%Y%m%d_%H%M") for spc in specconc_list]
@@ -19,53 +20,43 @@ class HIST_Translator(object):
 			specconc_list = [spc for spc,t in zip(specconc_list,ts) if (t>=timeperiod[0]) and (t<timeperiod[1]) and (t.hour % self.interval == 0)]
 		else:
 			specconc_list = [spc for spc,t in zip(specconc_list,ts) if (t>=timeperiod[0]) and (t<timeperiod[1])]
+		subdir_lists['SpeciesConc'] = specconc_list
 		if useStateMet:
 			met_list = glob(f'{self.hist_dir}/GEOSChem.StateMet*.nc4')
 			met_list.sort()
 			met_ts = [datetime.strptime(met.split('.')[-2][0:13], "%Y%m%d_%H%M") for met in met_list]
 			met_list = [met for met,t in zip(met_list,met_ts) if (t>=timeperiod[0]) and (t<timeperiod[1])]
+			subdir_lists['StateMet'] = met_list
 		if useLevelEdge:
 			le_list = glob(f'{self.hist_dir}/GEOSChem.LevelEdgeDiags*.nc4')
 			le_list.sort()
 			le_ts = [datetime.strptime(le.split('.')[-2][0:13], "%Y%m%d_%H%M") for le in le_list]
 			le_list = [le for le,t in zip(le_list,le_ts) if (t>=timeperiod[0]) and (t<timeperiod[1])]
-		if useStateMet and useLevelEdge:
-			return [specconc_list,le_list,met_list]
-		elif useStateMet:
-			return [specconc_list,met_list]
-		elif useLevelEdge:
-			return [specconc_list,le_list]
-		else:
-			return specconc_list
-	def combineHist(self,species,useLevelEdge=False, useStateMet = False):
+			subdir_lists['LevelEdgeDiags'] = le_list
+		return subdir_lists
+	def combineHist(self,useLevelEdge=False, useStateMet = False):
 		dataset=[]
-		if useLevelEdge and useStateMet:
-			specconc_list,le_list,met_list=self.globSubDir(self.timeperiod,useLevelEdge,useStateMet)
-			for specfile,lefile,metfile in zip(specconc_list,le_list,met_list):
-				hist_val = xr.load_dataset(specfile)[f'SpeciesConc_{species}']
-				lev_val = xr.load_dataset(lefile)[f'Met_PEDGE']
-				met_val = xr.load_dataset(metfile)[f'Met_AD']
-				data_val = xr.merge([hist_val, lev_val,met_val])
-				dataset.append(data_val)
-		elif useLevelEdge:
-			specconc_list,le_list=self.globSubDir(self.timeperiod,useLevelEdge,useStateMet)
-			for specfile,lefile in zip(specconc_list,le_list):
-				hist_val = xr.load_dataset(specfile)[f'SpeciesConc_{species}']
-				lev_val = xr.load_dataset(lefile)[f'Met_PEDGE']
-				data_val = xr.merge([hist_val, lev_val])
-				dataset.append(data_val)
-		elif useStateMet:
-			specconc_list,met_list=self.globSubDir(self.timeperiod,useLevelEdge,useStateMet)
-			for specfile,metfile in zip(specconc_list,met_list):
-				hist_val = xr.load_dataset(specfile)[f'SpeciesConc_{species}']
-				met_val = xr.load_dataset(metfile)[f'Met_AD']
-				data_val = xr.merge([hist_val, met_val])
-				dataset.append(data_val)
-		else:
-			specconc_list=self.globSubDir(self.timeperiod,useLevelEdge,useStateMet)
-			for specfile in specconc_list:
-				hist_val = xr.load_dataset(specfile)[f'SpeciesConc_{species}']
-				dataset.append(hist_val)
+		subdir_lists=self.globSubDir(self.timeperiod,useLevelEdge,useStateMet)
+		for ind, specfile in subdir_lists['SpeciesConc']:
+			hist_ds = xr.load_dataset(specfile)
+			hist_vals = []
+			le_vals = []
+			met_vals = []
+			for species in self.spc_config['HistorySpeciesConcToSave']:
+				hist_vals.append(hist_ds[f'SpeciesConc_{species}'])
+			if useLevelEdge:
+				lefile = subdir_lists['LevelEdgeDiags'][ind]
+				le_ds = xr.load_dataset(lefile)
+				for lecoll in self.spc_config['HistoryLevelEdgeDiagsToSave']:
+					le_vals.append(le_ds[lecoll])
+			if useStateMet:
+				metfile = subdir_lists['StateMet'][ind]
+				met_ds = xr.load_dataset(metfile)
+				for metcoll in self.spc_config['HistoryStateMetToSave']:
+					met_vals.append(met_ds[metcoll])
+			to_merge = hist_vals + le_vals + met_vals
+			data_val = xr.merge(to_merge)
+			dataset.append(data_val)
 		dataset = xr.merge(dataset)
 		return dataset
 	def getArea(self):
