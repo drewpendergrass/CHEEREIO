2be7553e2bd2fc9e82bde1285bbe573b04dd2480
Drew Pendergrass <pendergrassdrew@gmail.com>
2022-09-19 14:00:08 -0400

    initialize setup for either omi or tropomi now

--- core/setup_obs_dates.py
+++ core/setup_obs_dates.py
@@ -1,4 +1,13 @@
 import tropomi_tools as tt
+import omi_tools as ot
+import settings_interface as si 
 
-translator = tt.TROPOMI_Translator()
-_ = translator.initialReadDate()
+obs_types = list(set(si.getSpeciesConfig()["OBS_TYPE"].values))
+
+if "TROPOMI" in obs_types:
+	translator = tt.TROPOMI_Translator()
+	_ = translator.initialReadDate()
+
+if "OMI" in obs_types:
+	translator = ot.OMI_Translator()
+	_ = translator.initialReadDate()
