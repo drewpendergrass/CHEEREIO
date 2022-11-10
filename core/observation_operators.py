update default formula for error reduction to correct math error

core/observation_operators.py
@@ -10,7 +10,7 @@ import functools
 def produceSuperObservationFunction(fname):
 	if (fname is None) or (fname == "default"):
 		def super_obs(mean_error,num_obs,errorCorr=0,min_error=0,transportError=0):
-			return np.max([(mean_error * np.sqrt(((1-errorCorr)/num_obs) + errorCorr) )+transportError,min_error])
+			return np.max([np.sqrt((mean_error**2 * (((1-errorCorr)/num_obs) + errorCorr) )+transportError**2),min_error])
 	elif fname == "sqrt":
 		def super_obs(mean_error,num_obs,errorCorr=0,min_error=0):
 			return np.max([(mean_error * np.sqrt(((1-errorCorr)/num_obs) + errorCorr) ),min_error])