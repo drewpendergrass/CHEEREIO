a9c39fa2ec4694d2e9fea9224e5c8d1f36eb4065
Drew Pendergrass <pendergrassdrew@gmail.com>
2022-12-15 11:57:48 -0600

    fix date parsing

--- core/tropomi_tools.py
+++ core/tropomi_tools.py
@@ -155,7 +155,7 @@ def read_tropomi_acmg(filename, species, filterinfo=None, includeObsError = Fals
 	met['latitude'] = data['latitude_center'].values[goodvals] #nobs
 	timeraw = data['time'].values[goodvals,:] #nobs, ntime. Seven entries in format year', 'month', 'day', 'hour','minute', 'second'
 	#format as CHEEREIO-compliant string
-	timestring = [f'{str(int(timestamp[0]))}-{str(int(timestamp[1]).zfill(2))}-{str(int(timestamp[2])).zfill(2)}T{str(int(timestamp[3])).zfill(2)}:{str(int(timestamp[4])).zfill(2)}:{str(int(timestamp[5])).zfill(2)}Z' for timestamp in timeraw]
+	timestring = [f'{str(int(timestamp[0]))}-{str(int(timestamp[1])).zfill(2)}-{str(int(timestamp[2])).zfill(2)}T{str(int(timestamp[3])).zfill(2)}:{str(int(timestamp[4])).zfill(2)}:{str(int(timestamp[5])).zfill(2)}Z' for timestamp in timeraw]
 	met['utctime'] =  np.array(timestring)
 
 	met['column_AK'] = data['xch4_column_averaging_kernel'].values[goodvals,::-1] #nobs,layer
