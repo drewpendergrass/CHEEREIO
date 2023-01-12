a8a583dae7ee6be013a81e4d4b18964eed0e076d
Drew Pendergrass <pendergrassdrew@gmail.com>
2023-01-12 10:57:52 -0500

    finish cleaning up removal of non-normal/lognormal distributions

--- setup_ensemble_prelims.sh
+++ setup_ensemble_prelims.sh
@@ -354,7 +354,6 @@ grid_lev="$(jq -r ".LEVS" ens_config.json)"
 
 # Ensemble settings
 nEnsemble=$(jq -r ".nEnsemble" ens_config.json)
-pPERT="$(jq -r ".pPERT" ens_config.json)"
 
 dcr=$(jq -r ".DO_CONTROL_RUN" ens_config.json)
 dcer=$(jq -r ".DO_CONTROL_WITHIN_ENSEMBLE_RUNS" ens_config.json) #if true, we make a run directory without assimilation within the ensemble runs structure.
