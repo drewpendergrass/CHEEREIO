fix units

postprocess/postprocess_tools.py
@@ -128,6 +128,14 @@ def makeDatasetForEnsemble(ensemble_dir,species_names,timeperiod=None,hourlysub 
 		ds.to_netcdf(fullpath_output_name)
 	return ds
 
+def getArea(ensemble_dir,pp_dir):
+	subdirs,_,_ = globDirs(ensemble_dir,includeOutputDir=True,removeNature=True)
+	specconc_list = globSubDir(subdirs[0],None,1)
+	file = specconc_list[0]
+	ds = xr.open_dataset(file)
+	area = ds['AREA'].values
+	np.save(f'{pp_dir}/area.npy',area)
+
 def makeYEachAssimPeriod(path_to_bigy_subsets,assim_time,startdate=None,enddate=None,fullpath_output_name = None):
 	masterY = {}
 	bigy_list = glob(f'{path_to_bigy_subsets}/*.pkl')
@@ -218,7 +226,7 @@ def plotSurfaceMean(ds,species_name,outfile=None,unit='ppt',includesNature=False
 	enssd = ens.std(axis=0)
 	tsPlot(time,ensmean,enssd,species_name,unit,nature,outfile=outfile)
 
-def tsPlotTotalEmissions(ds_ensemble,ds_prior,collectionName,useLognormal = False, timeslice=None,outfile=None,conversion_factor=None):
+def tsPlotTotalEmissions(ds_ensemble,ds_prior,area,collectionName,useLognormal = False, timeslice=None,outfile=None,conversion_factor=None):
 	if timeslice is not None:
 		ds_ensemble = ds_ensemble.sel(time=slice(timeslice[0],timeslice[1]))
 		ds_prior = ds_prior.sel(time=slice(timeslice[0],timeslice[1]))
@@ -228,7 +236,9 @@ def tsPlotTotalEmissions(ds_ensemble,ds_prior,collectionName,useLognormal = Fals
 	else:
 		axis_to_average = (2,3) #Surface emissions only
 		prior_axis_to_average = (1,2)
-	da = ds_ensemble[collectionName].sum(axis=axis_to_average) #sum up all emissions
+	#Remove the area unit (e.g. kg/m2/s) by multiplying by area (m2)
+	da = ds_ensemble[collectionName]*area
+	da = da.sum(axis=axis_to_average) #sum up all emissions
 	enstime = np.array(ds_ensemble['time'])
 	if useLognormal:
 		ensmean = np.exp(np.mean(np.log(da),axis=0))

postprocess/postprocess_workflow.py
@@ -60,6 +60,13 @@ print('Scale factor postprocessing complete.')
 print('Starting HEMCO diagnostic (e.g. emissions) postprocessing.')
 
 try:
+	area = np.load(f'{pp_dir}/area.npy')
+except OSError:
+	print('Area postprocessing file not detected; generating now.')
+	pt.getArea(ens_dir,pp_dir)
+	area = np.load(f'{pp_dir}/area.npy')
+
+try:
 	hemcodiag = xr.open_dataset(f'{pp_dir}/combined_HEMCO_diagnostics.nc')
 except FileNotFoundError:
 	print('HEMCO diagnostic postprocessing file not detected; generating now.')
@@ -98,7 +105,7 @@ if useControl:
 		pt.combineHemcoDiagControl(control_dir,pp_dir,timeperiod)
 		hemcocontroldiag = xr.open_dataset(f'{pp_dir}/control_HEMCO_diagnostics.nc')
 	for collection in hemco_diags_to_process:
-		pt.tsPlotTotalEmissions(ds_ensemble=hemcodiag,ds_prior=hemcocontroldiag,collectionName=collection,useLognormal = lognormalErrors,timeslice=[POSTPROCESS_START_DATE,POSTPROCESS_END_DATE], outfile=f'{pp_dir}/timeseries_totalemissions_{collection}_against_prior.png')
+		pt.tsPlotTotalEmissions(ds_ensemble=hemcodiag,ds_prior=hemcocontroldiag,area=area,collectionName=collection,area = area,useLognormal = lognormalErrors,timeslice=[POSTPROCESS_START_DATE,POSTPROCESS_END_DATE], outfile=f'{pp_dir}/timeseries_totalemissions_{collection}_against_prior.png')
 	print('Control run HEMCO diagnostic (e.g. emissions) postprocessed and loaded.')
 
 if "calc850" in sys.argv: