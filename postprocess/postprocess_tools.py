4fb6d302e42e3676d4cff0631683cef609003748
Drew Pendergrass <pendergrassdrew@gmail.com>
2023-03-27 15:57:11 -0400

    make emissions plot consistent

--- postprocess/postprocess_tools.py
+++ postprocess/postprocess_tools.py
@@ -253,7 +253,7 @@ def tsPlotTotalEmissions(ds_ensemble,ds_prior,area,collectionName,useLognormal =
 		ensmean*=conversion_factor
 		enssd*=conversion_factor
 		da_prior*=conversion_factor
-	tsPlot(enstime,ensmean,enssd,collectionName,'kg/m2/s',priortime=priortime,prior=da_prior,priorname='control',outfile=outfile)
+	tsPlot(enstime,ensmean,enssd,collectionName,'kg/m2/s',priortime=priortime,prior=da_prior,priorcolor='r',outfile=outfile)
 
 def tsPlotSatCompare(bigY,species,numens,unit='ppb',observer_name='Observations',useControl=False,outfile=None):
 	ensmeans = []
@@ -311,7 +311,7 @@ def tsPlotSatCompare(bigY,species,numens,unit='ppb',observer_name='Observations'
 		plt.show()
 
 
-def tsPlot(time,ensmean,enssd,species_name,unit,nature=None,priortime=None,prior=None,priorname=None,outfile=None):
+def tsPlot(time,ensmean,enssd,species_name,unit,nature=None,priortime=None,prior=None,priorcolor='g',outfile=None):
 	plt.rcParams.update({'font.size': 16})
 	plt.figure(figsize=(6,4))
 	plt.plot(time,ensmean,color='b',label='Ensemble')
@@ -321,13 +321,7 @@ def tsPlot(time,ensmean,enssd,species_name,unit,nature=None,priortime=None,prior
 		plt.plot(time,nature,color='g',label='Nature')
 		plt.legend()
 	if prior is not None:
-		if priorname is not None:
-			if priorname=='control':
-				plt.plot(priortime,prior,color='r',label='Control')
-			else:
-				plt.plot(priortime,prior,color='g',label='Prior')
-		else:
-			plt.plot(priortime,prior,color='g',label='Prior')
+		plt.plot(priortime,prior,color=priorcolor,label='Prior')
 		plt.legend()
 	plt.xlabel('Time')
 	plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
