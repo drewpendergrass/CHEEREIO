19483bb42a4bc64674adb45d96e46ee943fead6b
Drew Pendergrass <pendergrassdrew@gmail.com>
2023-04-12 11:03:03 -0400

    add obspack switcher

---
+++ core/obspack_switch.py
@@ -0,0 +1,21 @@
+import settings_interface as si 
+import ruamel.yaml
+import sys
+import os.path
+
+spc_config = si.getSpeciesConfig()
+obspack_code = spc_config["ACTIVATE_OBSPACK"]
+obspack_file = spc_config["obspack_input_file"]
+path = str(sys.argv[1])
+
+yaml = ruamel.yaml.YAML()
+# yaml.preserve_quotes = True
+with open(f'{path}/geoschem_config.yml') as fp:
+	data = yaml.load(fp)
+
+#switch obspack to user setting
+data['extra_diagnostics']['obspack']['activate'] = obspack_code
+data['extra_diagnostics']['obspack']['input_file'] = obspack_file
+
+with open(f'{path}/geoschem_config.yml', "w") as f:
+	yaml.dump(data, f)
